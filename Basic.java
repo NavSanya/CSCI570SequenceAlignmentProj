b6fea0e3dfa2909cb38c91a5595a6424ab21b6c4
Abiola Johnson <abiolajohnson@Abiola.local>
2022-10-24 16:20:51 -0700

    second commit

---
+++ Basic.java
@@ -0,0 +1,36 @@
+public class Basic {
+    /* Global variables */
+    String str1;
+    String str2;
+    int[][] matrix;
+
+    /* Functions */
+    public void readFile() {
+
+    }
+
+    public void writeFile() {
+
+    }
+
+    public String generateString() {
+        return "";
+    }
+
+    private static double getMemory() {
+        return 0.0;
+    }
+
+    private static double getTime() {
+        return 0.0;
+    }
+
+    public double alignment() {
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
