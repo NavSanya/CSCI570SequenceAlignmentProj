a8e735b2f6f6e44d80703b1e1cf8ec04e636dddf
Abiola Johnson <abiolajohnson@usc-guestwireless-upc-newsc4509.usc.edu>
2022-10-31 21:44:55 -0700

    basic done

--- Basic.java
+++ Basic.java
@@ -10,10 +10,22 @@ public class Basic {
     int[][] matrix;
     Scanner scanner;
     StringBuilder sb;
+    int delta = 30;
+    Map<Character, Integer> charMap;
+    int[][] opt;
 
     public Basic() {
         this.map = new HashMap<>();
         this.sb = new StringBuilder();
+        matrix = new int[][] { { 0, 110, 48, 94 },
+                { 110, 0, 118, 48 },
+                { 48, 118, 0, 110 },
+                { 94, 48, 110, 0 } };
+        this.charMap = new HashMap<>();
+        charMap.put('A', 0);
+        charMap.put('C', 1);
+        charMap.put('G', 2);
+        charMap.put('T', 3);
     }
 
     /* Functions */
@@ -49,11 +61,11 @@ public class Basic {
                 str = sb.toString();
             }
             if (first) {
-                this.str1 = sb.toString();
+                this.str2 = sb.toString();
                 sb.setLength(0);
                 first = false;
             } else {
-                this.str2 = sb.toString();
+                this.str1 = sb.toString();
             }
         }
     }
@@ -71,8 +83,29 @@ public class Basic {
         return System.nanoTime() / 10e6;
     }
 
-    public double alignment() {
-        return 0.0;
+    public void alignment() {
+        opt = new int[str1.length()][str2.length()];
+
+        for (int i = 0; i < str1.length(); i++) {
+            opt[i][0] = delta * i;
+        }
+        for (int i = 0; i < str2.length(); i++) {
+            opt[0][i] = delta * i;
+        }
+        for (int i = 1; i < str1.length(); i++) {
+            for (int j = 1; j < str1.length(); j++) {
+                char c1 = str1.charAt(i - 1);/* changed */
+                char c2 = str2.charAt(j - 1);/* changed */
+                int a = charMap.get(c1);
+                int b = charMap.get(c2);
+                if (c1 == c2) {
+                    opt[i][j] = opt[i - 1][j - 1];
+                } else {
+                    opt[i][j] = Math.min(matrix[a][b] + opt[i - 1][j - 1],
+                            Math.min(delta + opt[i][j - 1], delta + opt[i - 1][j]));
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -81,6 +114,7 @@ public class Basic {
         Basic basic = new Basic();
         File file = new File("SampleTestCases/input1.txt");
         basic.readFile(file);
+        basic.alignment();
         double endSpace = getMemory();
         double endTime = getTime();
         double space = endSpace - startSpace;
@@ -88,6 +122,9 @@ public class Basic {
         System.out.println("Strings: ");
         System.out.println("String 1: " + basic.str1);
         System.out.println("String 2: " + basic.str2);
+        int n1 = basic.str1.length();/* changed */
+        int n2 = basic.str2.length();/* changed */
+        System.out.println(basic.opt[n1 - 1][n2 - 1]);/* changed */
         System.out.println("Complexity analysis: \nTime: " + time + " milliseconds\nSpace: " + space + " KB");
     }
 
